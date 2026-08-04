5e060e49246a6d5f34c3642cf7c90d59d36bf653
Saketh Bontu <sakbont@gmail.com>
2026-08-04 16:51:17 -0400

    Added metadata validation function

--- backend/book_loader.py
+++ backend/book_loader.py
@@ -4,4 +4,22 @@ from pathlib import Path
 def load_metadata(metadata_path: Path):
     with open(metadata_path, "r", encoding="utf-8") as file:
         metadata = json.load(file)
-    return metadata 
+    return metadata
+
+def validate_metadata(metadata):
+    required_fields = {
+        "id",
+        "title",
+        "author",
+        "language",
+        "total_chapters",
+        "chapters_directory",
+        "source_url",
+    }
+
+    missing_fields = required_fields - metadata.keys()
+
+    if missing_fields:
+        raise ValueError(
+            f"Missing required metadata fields: {sorted(missing_fields)}"
+        )
--- backend/tests/test_book_loader.py
+++ backend/tests/test_book_loader.py
@@ -1,5 +1,6 @@
-from backend.book_loader import load_metadata
+from backend.book_loader import load_metadata, validate_metadata
 import json
+import pytest
 
 def test_load_metadata(tmp_path):
     test_metadata_data = {
@@ -19,3 +20,16 @@ def test_load_metadata(tmp_path):
     metadata_dict = load_metadata(test_metadata)
 
     assert metadata_dict == test_metadata_data
+
+def test_validate_metadata():
+    test_metadata_data = {
+        "id" : "GOT",
+        "title" : "A Game of Thrones",
+        "author" : "George RR Martin",
+        "language" : "English",
+        "total_chapters" : 57,
+        "chapters_directory" : "chapters",
+    }
+
+    with pytest.raises(ValueError):
+        validate_metadata(test_metadata_data)
